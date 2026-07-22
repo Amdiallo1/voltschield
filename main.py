c44aafccf5e4fdb7e9d48b9286eb27ddb173d485
GEE <175382113+Amdiallo1@users.noreply.github.com>
2026-07-22 21:25:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,8 @@ app = FastAPI(title="VoltShield API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://voltshield.onrender.com",  # Production domain
-        "http://localhost:8000",           # Local testing
-        "http://127.0.0.1:8000"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
